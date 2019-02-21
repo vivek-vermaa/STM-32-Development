5d9342bd6c5312efbd7360ab34e332fd0790092a
Vivek Verma <tovivekverma@hotmail.com>
2019-02-21 10:50:23 -0800

    added msp initialization

--- Debug/Src/msp.d
+++ Debug/Src/msp.d
@@ -1,1 +1,76 @@
-Src/msp.o: ../Src/msp.c
+Src/msp.o: ../Src/msp.c \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/Hack\ Machine/Desktop/cube_mx/Default_STM_32/basic_peripheral/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
