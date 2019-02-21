82d89a6234b6f59c87f6ec6348345778545b3738
Vivek Verma <tovivekverma@hotmail.com>
2019-02-21 11:54:20 -0800

    MSP Init

--- Src/msp.c
+++ Src/msp.c
@@ -9,6 +9,13 @@
 
 void HAL_MSP_INIT()
 {
-	/*priority grouping for arm cortex*/
-	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+/*priority grouping for arm cortex*/
+HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+/*enable system exceptions of the cortex m4 microcotroller*/
+ SCB->SHCSR |= 0x7 <<16;
+
+ /*set interrupt priority*/
+ HAL_NVIC_SetPriority(MemoryManagement_IRQn,0,0);
+
 }
